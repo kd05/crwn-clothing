ca3b025f7f341d41366b58a8fa2fdd6529df3a79
krunal patel <krunalpatel5390@gmail.com>
2021-01-03 04:07:21 -0500

    update our menu items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,18 +50,11 @@ class Directory extends React.Component {
     render() {
         return (
             <div className='directory-menu'>
-
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => {
-                      return  <MenuItem
-                                    key={id}
-                                    title={title}
-                                    imageUrl={imageUrl}
-                                    size={size}
-                              />
+                    this.state.sections.map(({id, ...otherSectionProps}) => {
+                      return  <MenuItem key={id} {...otherSectionProps} />
                     })
                 }
-
             </div>
         );
     }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
 
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => {
+        return history.push(`${match.url}${linkUrl}`)
+    }}>
 
         <div
             className='background-image'
@@ -22,5 +24,5 @@ const MenuItem = ({title, imageUrl, size}) => (
 
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
